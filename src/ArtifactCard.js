95b0290461524e40e5a3972598c5a101e26d6537
Janne Väisänen <thejabbe@gmail.com>
2017-05-14 23:57:15 +0300

    Prototype contd.

--- src/ArtifactCard.js
+++ src/ArtifactCard.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import * as _ from 'ramda';
+import ReactMarkdown from 'react-markdown';
 import PlayerInfo from './PlayerInfo';
 import './ArtifactCard.css';
 
 class ArtifactCard extends Component {
   render() {
-    
+
     const artifact = this.props.artifact;
-    
+
     return (
       <div className="artifact-card">
         <div className="card-caption">
           <span>Artifact</span>
         </div>
         <div className="artifact-content">
-          <span className="small-caps">{artifact.name}</span>
-          <p>{artifact.text}</p>
+          <span className="small-caps centered">{artifact.name}</span>
+          <ReactMarkdown
+            className="sheet-text"
+            source={artifact.text}
+          />
         </div>
       </div>
     );
